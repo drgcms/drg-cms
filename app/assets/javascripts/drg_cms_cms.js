7df99f9c360ebe8f65c21b752bc51da6046675ec
Damjan Rems <rems@ozs.si>
2017-01-24 15:41:27 +0100

    Trying last el-finder.

--- app/assets/javascripts/drg_cms_cms.js
+++ app/assets/javascripts/drg_cms_cms.js
@@ -15,15 +15,24 @@
 //= require jquery-ui/widgets/droppable
 //= require jquery-ui/widgets/selectable
 
+//= require jquery-ui/widgets/mouse
+//= require jquery-ui/widgets/resizable
+//= require jquery-ui/widgets/selectable
+//= require jquery-ui/widgets/sortable
+
+//= require jquery-ui/widgets/accordion
+//= require jquery-ui/widgets/button
+//= require jquery-ui/widgets/datepicker
+//= require jquery-ui/widgets/dialog
+//= require jquery-ui/widgets/menu
+//= require jquery-ui/widgets/progressbar
+//= require jquery-ui/widgets/selectmenu
+//= require jquery-ui/widgets/slider
+//= require jquery-ui/widgets/spinner
+//= require jquery-ui/widgets/tabs
+
 //= require drg_cms/drg_cms
 //= require drg_cms/jquery.datetimepicker
-
-//= #require drg_cms/ckeditor/ckeditor.js
-//= #require drg_cms/ckeditor/lang/sl.js
-//= # require ckeditor_config
-
-//= #require drg_cms/elfinder/js/elfinder.min.js
-//= #require drg_cms/elfinder/js/proxy/elFinderSupportVer1.js
 
 //= require drg_cms/jquery.bpopup.min.js
 //= require drg_cms/select-multiple
