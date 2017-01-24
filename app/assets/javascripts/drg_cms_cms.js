c18952e30c73ed8f49b0f3d374146c937d8162d2
Damjan Rems <damjan.rems@gmail.com>
2017-01-24 23:19:19 +0100

    Optimizing for elfinder.

--- app/assets/javascripts/drg_cms_cms.js
+++ app/assets/javascripts/drg_cms_cms.js
@@ -15,22 +15,6 @@
 //= require jquery-ui/widgets/droppable
 //= require jquery-ui/widgets/selectable
 
-//= require jquery-ui/widgets/mouse
-//= require jquery-ui/widgets/resizable
-//= require jquery-ui/widgets/selectable
-//= require jquery-ui/widgets/sortable
-
-//= require jquery-ui/widgets/accordion
-//= require jquery-ui/widgets/button
-//= require jquery-ui/widgets/datepicker
-//= require jquery-ui/widgets/dialog
-//= require jquery-ui/widgets/menu
-//= require jquery-ui/widgets/progressbar
-//= require jquery-ui/widgets/selectmenu
-//= require jquery-ui/widgets/slider
-//= require jquery-ui/widgets/spinner
-//= require jquery-ui/widgets/tabs
-
 //= require drg_cms/drg_cms
 //= require drg_cms/jquery.datetimepicker
 
