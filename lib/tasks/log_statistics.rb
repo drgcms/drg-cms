21ad16b6ad3610ff9f3fbfa3dd791d9a6c82c3fd
Damjan Rems <damjan.rems@gmail.com>
2017-06-06 15:32:42 +0200

    Statistics are now generated only for days that are not already processed.

--- lib/tasks/log_statistics.rb
+++ lib/tasks/log_statistics.rb
@@ -14,7 +14,13 @@ end
 # Init some internal vars
 ###################################################################
 def init
-  @by_day = [] 
+  @by_day = {}
+  if File.exist?('result.txt')
+    File.readlines('result.txt').each do |line|
+      a = line.chomp.split("\t")
+      @by_day[a.first] = a
+    end
+  end  
 end
 
 ###################################################################
@@ -22,18 +28,20 @@ end
 # average response time.
 ###################################################################
 def response_by_days
-  i = 0
   selected_files.each do |file_name|
-    result = [file_name[0,6], 0, 0]
+    key = file_name[0,6]
+    next if @by_day[key]
+    result = [key, 0, 0, 0]
     File.readlines(file_name).each do |line|
       next unless line.match 'Completed 200 OK in'
       time = $'.split('ms').first.strip.to_i
       result[1] += 1
       result[2] += time
     end
+# average    
+    result[3] += result[2]/result[1]
     p result
-    @by_day << result
-#     break if (i+=1) > 4
+    @by_day[key] = result
   end
 end
 
@@ -41,7 +49,6 @@ end
 # Prints links which resultet in 404 error.
 ###################################################################
 def analyze_404
-  i = 0
   selected_files.each do |file_name|
     result = [file_name[0,6], 0, 0]
     url = ''
@@ -61,6 +68,5 @@ end
 
 init
 response_by_days
-p @by_day
-c = @by_day.inject('') {|result, e| result << e.join("\t") + "\n"} 
+c = @by_day.inject('') {|result, e| result << e.last.join("\t") + "\n"} 
 File.open('result.txt','w') {|f| f.write(c) }
