31509cfafae18309c79b7ecaee376d90f3730871
Damjan Rems <damjan.rems@gmail.com>
2017-08-11 15:15:24 +0200

    Start controls for editing settings data saved as yaml text in settings field on document.

--- app/controllers/dc_dummy_settings_control.rb
+++ app/controllers/dc_dummy_settings_control.rb
@@ -23,14 +23,14 @@
 #++
 
 ######################################################################
-# DrgcmsControls for DcPage model.
+# DrgcmsControls for DcDummy model for editing settings in a document.
 ######################################################################
-module DcPageControl
+module DcDummySettingsControl
 
 ######################################################################
 # Called when new empty record is created
 ######################################################################
-def dc_new_record()
+def _dc_new_record()
   @record.design_id = params[:design_id] if params[:design_id]
   return unless params[:page_id]
 #
@@ -39,5 +39,75 @@ def dc_new_record()
     @record.menu      = page.menu
   end
 end
+
+######################################################################
+# Called before edit. 
+# 
+# Loads fields on form with values from settings document.
+######################################################################
+def get_settings()
+# Check model  
+  begin
+    model = params[:table].classify.constantize    
+  rescue Exception => e 
+    flash[:error] = 'Invalid or undefined model name!'
+    return false    
+  end
+# Check fild name 
+  begin
+    doc = model.find(params[:id])
+    yaml = doc.send(params[:field_name])
+    yaml = '' if yaml.blank?
+  rescue Exception => e 
+    flash[:error] = 'Invalid or undefined field name!'
+    return false    
+  end
+# Check data
+  begin
+    data = YAML.load(yaml)
+  rescue Exception => e 
+    flash[:error] = 'Invalid configuration data found!'
+    return false    
+  end
+  [doc, data] 
+end
+
+######################################################################
+# Called before edit. 
+# 
+# Loads fields on form with values from settings document.
+######################################################################
+def dc_before_edit()
+  doc, data = get_settings
+  return false if doc.class == FalseClass
+#  
+  data['settings'].each { |key, value| @record[key] = value }
+  true
+end
+
+######################################################################
+# Called before save. 
+# 
+# Convert data from fields on form to yaml and save it to document field.
+######################################################################
+def dc_before_save()
+  doc, data = get_settings
+  return false if doc.class == FalseClass
+#
+  fields_on_form.each do |v|
+    session[:form_processing] = v['name'] # for debuging
+    next if v['type'].nil? or
+            v['type'].match('embedded') or # don't wipe embedded types
+            v['readonly'] or # fields with readonly option don't return value and would be wiped
+# return value from form field definition
+    value = DrgcmsFormFields.const_get(v['type'].camelize).get_data(params, v['name'])
+    data['settings'][v['name']] = value
+  end
+# save data to document field  
+  doc.send(params[:field_name], data.to_yaml)
+  doc.save
+  false # must be set
+end
+
 
 end 
