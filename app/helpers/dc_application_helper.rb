5a52c7877d632e8ffa846525aa0519b8e2242bde
Damjan Rems <damjan.rems@gmail.com>
2018-03-29 15:43:04 +0200

    Had to change parameter name.

--- app/helpers/dc_application_helper.rb
+++ app/helpers/dc_application_helper.rb
@@ -351,14 +351,15 @@ end
 # Similar to rails link_to, but also takes care of link icon, translation, ...
 ############################################################################
 def dc_link_to(caption, icon, parms, rest={})
+  icon_pos = 'first'
   if parms.class == Hash
     parms.stringify_keys!
     rest.stringify_keys!
     rest['class'] = rest['class'].to_s + ' dc-animate'
     rest['target'] ||=  parms.delete('target')
     parms['controller'] ||= 'cmsedit'
+    icon_pos = parms.delete('icon_pos') || 'first'
   end
-  icon_pos = parms.delete('icon') || 'first'
 #  
   if icon
     icon_image = if icon.match(/\./)
