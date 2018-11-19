cd7f9767805ef953c300ba0c83e386172bb9fe30
Damjan Rems <damjan.rems@gmail.com>
2018-11-19 09:56:11 +0100

    Version 0.5.52.15. Collection for list of removed urls dc_remove_url was added to project. Table can hold urls removed from site and can be used when creating sitemap.

--- app/models/dc_removed_url.rb
+++ app/models/dc_removed_url.rb
@@ -1,5 +1,5 @@
 #--
-# Copyright (c) 2012+ Damjan Rems
+# Copyright (c) 2018+ Damjan Rems
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -21,7 +21,34 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #++
 
-module DrgCms #:nodoc:
-  # drg_cms gem version
-  VERSION = "0.5.52.15"
+##########################################################################
+# == Schema information
+#
+# Collection name: dc_removed_url : List of URL-s removed from site
+#
+#  _id                  BSON::ObjectId       _id
+#  created_at           Time                 created_at
+#  updated_at           Time                 updated_at
+#  url                  String               URL that is no longer active
+#  dc_site_id           Object               URL is valid only for the site
+# 
+#  List of URLs removed from site so web crawlers will also remove them from their 
+#  indexes. Usefull when creating sitemaps.
+#    
+##########################################################################
+class DcRemovedUrl
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  field :url,    type: String
+  field :description, type: String
+  
+  field :created_by,  type: BSON::ObjectId
+  field :updated_by,  type: BSON::ObjectId
+  
+  belongs_to  :dc_site, optional: true
+  
+  index( dc_site: 1 )
+  
+  validates :url, presence: true
 end
