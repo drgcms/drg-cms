8e6327b87ff84a7d454fc006d001fe1813c44509
Rems <damjan.rems@ozs.si>
2020-12-10 12:45:03 +0100

    is_id: false is much better then not_id: true

--- app/models/drgcms_form_fields/text_autocomplete.rb
+++ app/models/drgcms_form_fields/text_autocomplete.rb
@@ -30,7 +30,7 @@ module DrgcmsFormFields
 # * +type:+ text_autocomplete (required)
 # * +table+ Collection (table) name. When defined search must contain field name
 # * +with_new+ Will add an icon for shortcut to add new document to collection
-# * +not_id+ Field value represent value not an id. Field will save entered value when not selected wit autocomplete.
+# * +is_id+ Field value represent value as id. If false, field will use entered value and not value selected with autocomplete. Default is true.
 # * +search:+ Search may consist of three parameters from which are separated either by dot (.) 
 #   * search_field_name; when table option is defined search must define field name which will be used for search query
 #   * collection_name.search_field_name; Same as above except that table options must be ommited.
@@ -42,6 +42,7 @@ module DrgcmsFormFields
 #      name: user_id
 #      type: text_autocomplete
 #      search: dc_user.name
+#      is_id: false
 #      html:
 #        size: 30
 ###########################################################################
@@ -51,8 +52,8 @@ class TextAutocomplete < DrgcmsField
 # Render text_autocomplete field html code
 ###########################################################################
 def render
-# Return descriptive text and put it into input field
-# search field name
+  # Return descriptive text and put it into input field
+  # search field name
   if @yaml['search'].class == Hash
     table    = @yaml['search']['table']
     ret_name = @yaml['search']['field']
@@ -62,41 +63,40 @@ def render
   else
     ret_name = @yaml['search']
   end
-# determine table name 
+  # determine table name
   if @yaml['table']
     table = if @yaml['table'].class == String
       @yaml['table']
-# eval(how_to_get_my_table_name)    
     elsif @yaml['table']['eval']
       eval @yaml['table']['eval']
     else
-      Rails.logger.error "Field #{@yaml['name']}: Invalid table parameter!"
+      Rails.logger.error "Field #{ @yaml['name'] }: Invalid table parameter!"
       nil
     end
   end
   return 'Table or field keyword not defined!' unless (table and ret_name)
-# TODO check if table exists 
+  # TODO check if table exists
   t = table.classify.constantize
-# find record and return value of field
+  # find record and return value of field
   value_send_as = 'p_' + @yaml['name']
   value = if @parent.params[value_send_as]
     @parent.params[value_send_as]
   elsif @record and @record[@yaml['name']]
     @record[@yaml['name']]
   end
-# Found value to be written in field. If field is not found write out value.
+  # Found value to be written in field. If field is not found write out value.
   if value
     record = t.find(value) unless @yaml['not_id'] # don't if it's is not an id
     value_displayed = record ? record.send(ret_name) : value
   end
-# return if readonly
+  # return if readonly
   return ro_standard(value_displayed) if @readonly
-# Add method back, so autocomplete will know that it must search for method inside class
+  # Add method back, so autocomplete will know that it must search for method inside class
   ret_name = "#{ret_name}.#{method}" if method
   @yaml['html'] ||= {}
   @yaml['html']['value'] = value_displayed
   @yaml['html']['placeholder'] ||= t('drgcms.search_placeholder') || nil
-#    
+  #
   _name = '_' + @yaml['name']
   record = record_text_for(@yaml['name'])  
   @html << '<span class="dc-text-autocomplete">' + @parent.text_field(record, _name, @yaml['html']) + '<span></span>'
@@ -106,15 +106,15 @@ def render
              style: "vertical-align: top;", 'data-table' => @yaml['with_new'] )    
   end
   @html << '</span>' + @parent.hidden_field(record, @yaml['name'], value: value)        # actual value will be in hidden field
-# JS stuff
-# allow unselected values on not_id: true option
-  not_id_code = %Q[
+  # JS stuff
+  # allow unselected values on is_id: false option
+  not_id = @parent.dc_dont?(@yaml['is_id'], false)
+  not_id_code = %(
 if (ui.item == null) {  
 $("##{record}_#{@yaml['name']}").val($("##{record}__#{@yaml['name']}").val() );
 return;
-}
-] if @yaml['not_id']             
-#  
+} ) if not_id
+  #
   @js << <<EOJS
 $(document).ready(function() {
   $("##{record}_#{_name}").autocomplete( {
