c77191be4a2b7359802234514cbc7db43bf97bcc
Rems <damjan.rems@ozs.si>
2020-12-17 13:13:49 +0100

    Enable parameters to be assigned as default values to poll field. Use as: default: params,id

--- app/renderers/dc_poll_renderer.rb
+++ app/renderers/dc_poll_renderer.rb
@@ -29,7 +29,7 @@ class DcPollRenderer
   
 include CmsCommonHelper
 include DcApplicationHelper
-include ActionView::Helpers::FormHelper # for form hepers
+include ActionView::Helpers::FormHelper # for form helpers
 include ActionView::Helpers::FormOptionsHelper # for select helper
 
 ########################################################################
@@ -57,7 +57,7 @@ def do_one_item(poll, yaml)
   html = ''
   yaml['separator'] ||= ''
   yaml['text']      ||= ''
-# 
+  # label
   text = yaml['text'].match(/\./) ? t(yaml['text']) : yaml['text'] 
   if yaml['mandatory']
     text << ( poll.display == 'in' ? ' *' : '<font color="red"> *</font>' )
@@ -67,7 +67,7 @@ def do_one_item(poll, yaml)
     text << " &nbsp;" if poll.display == 'lr' and !yaml['type'].match(/submit_tag|link_to/)
   end
   
-# Just add text if comment and go to next one    
+  # Just add text if comment and go to next one
   if yaml['type'] == 'comment'
     html << if poll.display == 'lr'
       "<div class='row-div'><div class='dc-form-label poll-data-text comment'>#{text}</div></div>"
@@ -76,35 +76,37 @@ def do_one_item(poll, yaml)
     end    
     return html
   end
-# Set default value, if not already set
+
+  # Set default value, if not already set
   if yaml['default'] 
     if yaml['default'].match('eval')
       e = yaml['default'].match(/\((.*?)\)/)[1]
       yaml['default'] = eval e
+    elsif yaml['default'].match('params')
+      param_name = yaml['default'].split(/\.|\ |\,/)[1]
+      yaml['default'] = @parent.params[param_name]
     end
     key = "p_#{yaml['name']}"
     params[key] = yaml['default'] unless params[key]
   end
-# Label as placeholder
+  # Label as placeholder
   if poll.display == 'in'
     yaml['html'] ||= {}
     yaml['html']['placeholder'] = text
   end
-# create form_field object and retrieve html code
+  # create form_field object and retrieve html code
   clas_string = yaml['type'].camelize
   field_html = if DrgcmsFormFields.const_defined?(clas_string) 
     clas = DrgcmsFormFields.const_get(clas_string)
     field = clas.new(@parent, @record, yaml).render
-#TODO collect all javascript and add it at the end
     @part_js << field.js
-    field.html #+ (field.js.size > 0 ? @parent.javascript_tag(field.js) : '')
+    field.html
   else # error string
     "Error: Code for field type #{yaml['type']} not defined!"
   end
 
-#  field = send('dc_' + yaml['type'], yaml) # call dc_xxx method to get field html code
   if yaml['type'].match(/submit_tag|link_to/)
-# There can be more than one links on form. End the data at first link or submit.
+    # There can be more than one links on form. End the data at first link or submit.
     if !@end_of_data 
       html << if poll.display == 'lr'
         "</div><br>\n"
@@ -120,10 +122,10 @@ def do_one_item(poll, yaml)
       end
       @end_of_data = true
     end
-# submit and link tag 
+    # submit and link tag
     clas = yaml['type'].match(/submit_tag/) ? '' : 'dc-link-submit'
     html << "<span class='#{clas} dc-animate'>#{field_html}#{yaml['separator']}</span>"
-# other fields
+  # other fields
   else
     html << case
       when poll.display == 'lr' then
@@ -158,20 +160,19 @@ end
 # Default poll renderer method. Renders data for specified pool.
 ########################################################################
 def default
-# poll_id may be defined in params or opts
+  # poll_id may be defined in params or opts
   poll_id = @opts[:poll_id] || @parent.params[:poll_id]
   return '<br>Poll id is not defined?<br>' if poll_id.nil?
-#  
+
   poll = DcPoll.find(poll_id)
   poll = DcPoll.find_by(name: poll_id) if poll.nil? # name instead of id
   return "<div class=\"dc-form-error\">Invalid Poll id #{poll_id}</div>" if poll.nil?
-# If parent cant be seen. so cant be polls
+  # If parent cant be seen. so cant be polls
   can_view, message = dc_user_can_view(@parent, @parent.page)
   return "<div class=\"dc-form-error\">#{message}</div>" unless can_view
-  
+
   html = @opts[:div] ? "<div id='#{@opts[:div]}'>" : ''
   html << '<a name="poll-top"></a>'
-  #
   unless poll.pre_display.blank?
     begin
       continue, message = eval_pre_display(poll.pre_display)
@@ -179,48 +180,49 @@ def default
       return "<div class=\"dc-form-error\">Error! Poll pre display. Error: #{e.message}</div>" 
     end
     return message unless continue
+
     html << message if message
   end
-# there might be more than one poll displayed on page. Check if messages and values are for me
-  if @parent.flash[:poll_id].nil? or @parent.flash[:poll_id].to_s == poll_id.to_s
-# If flash[:record] is present copy content to params record hash
+  # there might be more than one poll displayed on page. Check if messages and values are for me
+  if @parent.flash[:poll_id].nil? || @parent.flash[:poll_id].to_s == poll_id.to_s
+    # If flash[:record] is present copy content to params record hash
     @parent.flash[:record].each {|k,v| @parent.params["p_#{k}"] = v } if @parent.flash[:record]  
-# Error during procesing request
+    # Error during procesing request
     html << "<div class=\"dc-form-error\">#{@parent.flash[:error]}</div>\n" if @parent.flash[:error].to_s.size > 0
     html << "<div class=\"dc-form-info\">#{@parent.flash[:info]}</div>\n" if @parent.flash[:info]
   end
-# div and form tag
+  # div and form tag
   html <<  "<div class=\"poll-div\">\n"
-# edit link  
+  # edit link
   if @opts[:edit_mode] > 1
     @opts[:editparams].merge!( controller: 'cmsedit', action: 'edit', id: poll._id, table: 'dc_poll', form_name: 'dc_poll' )
     @opts[:editparams].merge!(title: "#{t('drgcms.edit')}: #{poll.name}")
     @opts[:editparams].delete(:ids) # this is from page, but it gets in a way
     html << dc_link_for_edit( @opts[:editparams] )
   end
-#  
+
   html << case
   when poll.operation == 'poll_submit' then
     @parent.form_tag(action: poll.operation, method: :put)
   when poll.operation == 'link' then
     @parent.form_tag( poll.parameters, method: :put)
   end
-# header 
-  html << "<div class='poll-title'>#{poll.title}</div>" unless poll.title[0] == '-' # - on first position will not display title
-  html << poll.sub_text.to_s # if poll.sub_text.to_s.size > 5
+  # header, - on first position will not display title
+  html << "<div class='poll-title'>#{poll.title}</div>" unless poll.title[0] == '-'
+  html << poll.sub_text.to_s
   html << if poll.display == 'lr'
     "\n" + '<div class="poll-data-table">'
   else
     '<div class="poll-data-div">' + "\n"
   end
-# items. Convert each item to yaml
+  # items. Convert each item to yaml
   @end_od_data = false
   if poll.form.to_s.size < 10
     items = poll.dc_poll_items
-    items.sort! {|a,b| a.order <=> b.order }
+    items.sort! { |a,b| a.order <=> b.order }
     items.each do |item|
       next unless item.active # disabled items
-  # convert options to yaml
+      # convert options to yaml
       yaml = YAML.load(item.options) || {}
       yaml = {} if yaml.class == String
       yaml['name'] = item.name
@@ -228,18 +230,17 @@ def default
       yaml['html']['size'] = item.size
       (yaml['html']['class'] ||= 'dc-submit') if item.type == 'submit_tag'
       yaml['text'] = item.text
-      yaml['mandatory']  = item.mandatory
-      yaml['type']       = item.type
+      yaml['mandatory'] = item.mandatory
+      yaml['type'] = item.type
       html << do_one_item(poll, yaml)
     end
-# Form. Just call do_one_item for each form item
+  # FORM. Just call do_one_item for each form item
   else
     yaml = YAML.load(poll.form.gsub('&nbsp;',' ')) # very annoying. They come with copy&paste ;-)
-# if entered without numbering yaml is returned as Hash otherwise as Array
+    # if entered without numbering yaml is returned as Hash otherwise as Array
     yaml.each { |i| html << do_one_item(poll, (i.class == Hash ? i : i.last)) } # 
   end
-# hide some fields usefull as parameters  
-# was html << @parent.hidden_field_tag('return_to', @opts[:return_to] || @parent.params[:return_to] || '/')
+  # hide some fields usefull as parameters
   html << @parent.hidden_field_tag('return_to', @opts[:return_to] || @parent.params[:return_to] || @parent.request.url)
   html << @parent.hidden_field_tag('return_to_error', @parent.request.url )
   html << @parent.hidden_field_tag('poll_id', poll_id )
